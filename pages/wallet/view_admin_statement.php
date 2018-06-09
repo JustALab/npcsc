cdd10ea760ea02f8008f21fb0dfd47b5a400b519
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-06-09 14:05:08 +0530

    Wallet statements

--- pages/wallet/view_admin_statement.php
+++ pages/wallet/view_admin_statement.php
@@ -5,45 +5,80 @@
   }
   include '../header_nav.php';
   include '../sidebar.php';
-?>
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Wallet Statement</h3>
-
-          <!-- <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
+  include '../../services/constants.php';
+  ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Main content -->
+  <section class="content">
+    <br />
+    <!-- Default box -->
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Wallet Statement</h3>
+        <!-- <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+            <i class="fa fa-minus"></i></button>
+          <button type="button" class="btn btn-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+            <i class="fa fa-times"></i></button>
           </div> -->
-        </div>
-        <div class="card-body">
-          Admin statement
-        </div>
-        <!-- /.card-body -->
-        <!-- <div class="card-footer">
-          Footer
-        </div> -->
-        <!-- /.card-footer-->
       </div>
-      <!-- /.card -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <?php 
-    include '../footer_imports.php';
+      <div class="card-body">
+        <table id="wallet_statement_table" class="table">
+          <thead>
+            <tr>
+              <th>S. No.</th>
+              <th>Transaction ID</th>
+              <th>Date Time</th>
+              <th>Description</th>
+              <th>Previous Balance</th>
+              <th>Transaction Type</th>
+              <th>Balance</th>
+              <th>Agent ID</th>
+              <th>Agent Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              $query = 'SELECT wt.transaction_id, wt.date_time, wt.description, wt.previous_balance, wt.transaction_type, wt.balance, u.user_id, u.name FROM '.TABLE_WALLET_TRANS.' wt, '.TABLE_WALLET.' w, '.TABLE_USERS.' u where wt.wallet_id=w.wallet_id AND w.user_id=u.user_id  ORDER BY transaction_id DESC';
+              $result = mysqli_query($dbc, $query);
+              if(mysqli_num_rows($result) > 0){
+                $rowCount = 0;
+                while($row = mysqli_fetch_assoc($result)){
+                  echo "<tr>";
+                  echo "<td>".++$rowCount."</td>";
+                  echo "<td>".$row['transaction_id']."</td>";
+                  echo "<td>".$row['date_time']."</td>";
+                  echo "<td>".$row['description']."</td>";
+                  echo "<td>".$row['previous_balance']."</td>";
+                  echo "<td>".$row['transaction_type']."</td>";
+                  echo "<td>".$row['balance']."</td>";
+                  echo "<td>".$row['user_id']."</td>";
+                  echo "<td>".$row['name']."</td>";
+                  echo "</tr>";
+                }
+              }
+              ?>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.card-body -->
+      <!-- <div class="card-footer">
+        Footer
+        </div> -->
+      <!-- /.card-footer-->
+    </div>
+    <!-- /.card -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php 
+  include '../footer_imports.php';
   ?>  
-  <script type="text/javascript">
-  </script>
-  <?php 
-    include '../footer.php';
+<script type="text/javascript">
+  $('#wallet_statement_table').DataTable();
+</script>
+<?php 
+  include '../footer.php';
   ?>
