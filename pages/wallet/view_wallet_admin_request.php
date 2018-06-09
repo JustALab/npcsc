8497778623ab70d0f921d1905b210a3a72c73f3f
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-06-09 11:42:04 +0530

    Wallet Request - Admin
    User details displayed in wallet request admin page.

--- pages/wallet/view_wallet_admin_request.php
+++ pages/wallet/view_wallet_admin_request.php
@@ -10,9 +10,14 @@
   include 'bank_config.php';
 
   $requestId = $_GET['request_id'];
-  $query = "SELECT * FROM ".TABLE_WALLET_REQUESTS." WHERE request_id='$requestId'";
+  $query = "SELECT u.user_id, u.name, wr.request_id, wr.to_bank_name, wr.transaction_type, wr.request_amount, wr.request_date, wr.bank_name, wr.bank_name, wr.reference_no, wr.status FROM ".TABLE_WALLET_REQUESTS." wr, ".TABLE_WALLET." w, ".TABLE_USERS." u WHERE wr.wallet_id=w.wallet_id AND w.user_id=u.user_id AND request_id='$requestId'";
   $result = mysqli_query($dbc, $query);
   $row = mysqli_fetch_assoc($result);
+
+  $userId = $row['user_id'];
+  $userQuery = "SELECT * FROM ".TABLE_USERS." WHERE user_id='$userId'";
+  $userResult = mysqli_query($dbc, $userQuery);
+  $userRow = mysqli_fetch_assoc($userResult); 
 ?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -82,8 +87,8 @@
           <input type="hidden" name="user_id" value="<?php echo $requestId; ?>">
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
         <?php if($row['status'] == 'Pending'){ ?>
+        <div class="card-footer">
           <div id="approve_reject_row" class="row">
             <div class="col-sm-6 col-md-4 col-lg-4">
               <button class="btn btn-block btn-danger" onclick="updateRequestStatus(<?php echo $requestId; ?>, 'Denied');">Deny</button>
@@ -92,9 +97,70 @@
               <button class="btn btn-block btn-success" onclick="updateRequestStatus(<?php echo $requestId; ?>, 'Approved');">Approve</button>
             </div>
           </div>
-        <?php } ?>
         </div>
+        <?php } ?>
         <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
+      <div class="card">
+       <div class="card-header">
+          <h3 class="card-title">Agent Name: <?php echo $userRow['name']; ?></h3>
+
+          <div class="card-tools">
+          <?php if($userRow['status'] == 'Approved'){ ?>
+            <button type="button" class="btn btn-block btn-success btn-sm btn-flat" disabled>Approved</button>
+          <?php } ?>
+          <?php if($userRow['status'] == 'Denied'){ ?>
+            <button type="button" class="btn btn-block btn-danger btn-sm btn-flat" disabled>Denied</button>
+          <?php } ?>
+          <?php if($userRow['status'] == 'Pending'){ ?>
+            <button type="button" class="btn btn-block btn-warning btn-sm btn-flat" disabled>Pending</button>
+          <?php } ?>
+          <?php if($userRow['status'] == 'Blocked'){ ?>
+            <button type="button" class="btn btn-block btn-danger btn-sm btn-flat" disabled>Blocked</button>
+          <?php } ?>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-4">
+              <div class="form-group">
+                <label>Email:</label>
+                <input type="text" id="" name="" class="form-control" disabled value="<?php echo $userRow['email']; ?>">
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+              <div class="form-group">
+                <label>Mobile:</label>
+                <input type="text" id="" name="" class="form-control" disabled value="<?php echo $userRow['mobile']; ?>">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-4">
+              <div class="form-group">
+                <label>PAN Number:</label>
+                <input type="text" id="" name="" class="form-control" disabled value="<?php echo $userRow['pan_no']; ?>">
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+              <div class="form-group">
+                <label>Aadhaar Number:</label>
+                <input type="text" id="" name="" class="form-control" disabled value="<?php echo $userRow['aadhaar_no']; ?>">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-12 col-xs-12 col-md-8 col-lg-8">
+              <div class="form-group">
+                <label>Address :</label>
+                <textarea id="" name="" class="form-control" disabled><?php echo $userRow['address']; ?></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.card-body -->
       </div>
       <!-- /.card -->
 
