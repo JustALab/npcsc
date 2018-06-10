8c83d47b86fbb71dcb97a3d738a3c2a600624ded
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-06-10 15:42:52 +0530

    SQL commit

--- sql/narpavicsc-structure.sql
+++ sql/narpavicsc-structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 10, 2018 at 12:00 PM
+-- Generation Time: Jun 10, 2018 at 12:12 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
@@ -58,19 +58,95 @@ CREATE TABLE `pan_application` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `pan_application`
+-- Table structure for table `price_config`
 --
 
-INSERT INTO `pan_application` (`application_no`, `status`, `application_date`, `application_type`, `pan_number`, `applicant_category`, `applicant_title`, `applicant_fname`, `applicant_mname`, `applicant_lname`, `father_fname`, `father_mname`, `father_lname`, `name_on_card`, `dob`, `contact_details`, `email`, `name_as_per_aadhaar`, `proof_of_id`, `proof_of_address`, `proof_of_dob`, `gender`, `aadhaar_no`, `flat_door_block_no`, `premises_building_village`, `road_street_lane_postoffice`, `area_taluk_subdivision`, `town_district`, `state_ut`, `pin_code`, `photo_file_name`, `signature_file_name`, `document_file_name`, `receipt_file_name`, `user_id`) VALUES
-(1, 'Approved', '09/06/2018', 'New Application', '', 'Individual', 'Shri', 'Sanjith', 'Shan', '22', 'Daddy', 'Daddy', 'Daddy', 'Sanjith Shan', '02/02/1950', '9876543210', 'sanjithshan22@gmail.com', 'Sanjith Shan', 'Aadhar', 'Aadhar', 'Aadhaar', 'Male', '987654321010', '12', 'qqwwe', 'qqww', 'qwww', 'qqqqq', '2', '643217', '2_photo_20180609151611.jpg', '2_sign_20180609151611.jpeg', '2_doc_20180609151611.pdf', '1_receipt.pdf', 2),
-(2, 'Denied', '09/06/2018', 'New Application', '', 'Individual', 'Shri', 'Sanjith', 'Shan', '22', 'Daddy', 'Daddy', 'Daddy', 'Sanjith Shan', '02/02/1950', '9876543210', 'sanjithshan22@gmail.com', 'Sanjith Shan', 'Aadhar', 'Aadhar', 'Aadhaar', 'Male', '987654321010', '12', 'qqwwe', 'qqww', 'qwww', 'qqqqq', '2', '643217', '2_photo_20180609151618.jpg', '2_sign_20180609151618.jpeg', '2_doc_20180609151618.pdf', '', 2),
-(3, 'Approved', '09/06/2018', 'New Application', '', 'Individual', 'Shri', 'Sanjith', 'Shan', '22', 'Daddy', 'Daddy', 'Daddy', 'Sanjith Shan', '02/02/1950', '9876543210', 'sanjithshan22@gmail.com', 'Sanjith Shan', 'Aadhar', 'Aadhar', 'Aadhaar', 'Male', '987654321010', '12', 'qqwwe', 'qqww', 'qwww', 'qqqqq', '2', '643217', '2_photo_20180609152748.jpg', '2_sign_20180609152748.jpeg', '2_doc_20180609152748.pdf', '', 2),
-(4, 'Approved', '09/06/2018', 'New Application', '', 'Individual', 'Shri', 'Sanjith', 'Shan', '', 'Daddy', 'Daddy', '', 'Sanjith Shan', '02/02/2000', '9876543210', 'sanjithshan22@gmail.com', 'Sanjith Shan', 'Aadhar', 'Aadhar', 'Aadhaar', 'Male', '983993933456', '12', '113', '1233', '1233', 'Kotagiri', '1', '643217', '2_photo_20180609162130.jpg', '2_sign_20180609162130.jpeg', '2_doc_20180609162130.pdf', '4_receipt.pdf', 2),
-(5, 'Pending', '09/06/2018', 'New Application', '', 'Individual', 'Shri', 'Sanjith', 'Shan', '', 'Daddy', 'Daddy', '', 'Sanjith Shan', '02/02/2000', '9876543210', 'sanjithshan22@gmail.com', 'Sanjith Shan', 'Aadhar', 'Aadhar', 'Aadhaar', 'Male', '983993933456', '12', '113', '1233', '1233', 'Kotagiri', '1', '643217', '2_photo_20180609162147.jpg', '2_sign_20180609162147.jpeg', '2_doc_20180609162147.pdf', '', 2),
-(6, 'Pending', '10/06/2018', 'New Application', '', 'Individual', 'Shri', 'dummy', 'd', 'dd', 'dummy', 'd', 'dd', 'dummy', '12/06/1994', '8190912853', 'rakshanakr@gmail.com', 'Deivaraja Ramasamy', 'Aadhar', 'Aadhar', 'Driving License', 'Male', '123455555555', 'qw1212', '#6/160, Riffle Range, Donnington', '#6/160, Riffle Range, Donnington', 'asasas', 'Kotagiri', '2', '643217', '2_photo_20180609183148.jpg', '2_sign_20180609183148.jpg', '2_doc_20180609183148.pdf', '', 2),
-(7, 'Pending', '02/02/2020', 'New Application', '', 'Individual', 'Shri', 'dum', 'dum', 'dum', 'dum', 'dum', 'dum', 'nam', '02/02/2020', '9992020', 'sanjithshan22@gmail.com', 'dummy', 'Aadhar', 'Aadhar', 'Birth Certificate', 'Female', '987654321011', '11', '11', '11', '11', '11', '2', '111111', '2_photo_20180609183834.jpeg', '2_sign_20180609183834.jpeg', '2_doc_20180609183834.pdf', '', 2),
-(8, 'Pending', '20/02/2020', 'New Application', '', 'Individual', 'Shri', 'test', 'test', '', 'test', '', 'Ramasamy', 'test', '03/03/2030', '777499', 'rram.ramasamy@gmail.com', 'ram', 'Aadhar', 'Aadhar', 'Aadhaar', 'Male', '778899992001', '223', 'Vadavalli', 'Vadavalli', '33', 'Coimbatore', '2', '641046', '2_photo_20180610121158.jpg', '2_sign_20180610121158.jpg', '2_doc_20180610121158.pdf', '', 2);
+CREATE TABLE `price_config` (
+  `price_config_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `amount` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `service_id` int(11) NOT NULL,
+  `service_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_type` varchar(10) NOT NULL DEFAULT 'AGENT',
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `aadhaar_no` varchar(12) NOT NULL,
+  `pan_no` varchar(10) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Pending',
+  `user_location` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet`
+--
+
+CREATE TABLE `wallet` (
+  `wallet_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` varchar(15) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet_requests`
+--
+
+CREATE TABLE `wallet_requests` (
+  `request_id` int(11) NOT NULL,
+  `to_bank_name` varchar(50) NOT NULL,
+  `transaction_type` varchar(20) NOT NULL,
+  `request_amount` varchar(15) NOT NULL,
+  `request_date` varchar(10) NOT NULL,
+  `bank_name` varchar(30) NOT NULL,
+  `reference_no` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Pending',
+  `wallet_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet_transactions`
+--
+
+CREATE TABLE `wallet_transactions` (
+  `transaction_id` int(11) NOT NULL,
+  `date_time` varchar(20) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `previous_balance` varchar(15) NOT NULL,
+  `transaction_type` varchar(6) NOT NULL,
+  `amount` varchar(20) NOT NULL,
+  `balance` varchar(15) NOT NULL,
+  `wallet_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -84,6 +160,47 @@ ALTER TABLE `pan_application`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `price_config`
+--
+ALTER TABLE `price_config`
+  ADD PRIMARY KEY (`price_config_id`),
+  ADD UNIQUE KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`service_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `email` (`email`);
+
+--
+-- Indexes for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD PRIMARY KEY (`wallet_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `wallet_requests`
+--
+ALTER TABLE `wallet_requests`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `wallet_id` (`wallet_id`);
+
+--
+-- Indexes for table `wallet_transactions`
+--
+ALTER TABLE `wallet_transactions`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `wallet_id` (`wallet_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -93,6 +210,36 @@ ALTER TABLE `pan_application`
 ALTER TABLE `pan_application`
   MODIFY `application_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
+-- AUTO_INCREMENT for table `price_config`
+--
+ALTER TABLE `price_config`
+  MODIFY `price_config_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `wallet`
+--
+ALTER TABLE `wallet`
+  MODIFY `wallet_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `wallet_requests`
+--
+ALTER TABLE `wallet_requests`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `wallet_transactions`
+--
+ALTER TABLE `wallet_transactions`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
 -- Constraints for dumped tables
 --
 
@@ -101,3 +248,27 @@ ALTER TABLE `pan_application`
 --
 ALTER TABLE `pan_application`
   ADD CONSTRAINT `pan_application_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `price_config`
+--
+ALTER TABLE `price_config`
+  ADD CONSTRAINT `price_config_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `services` (`service_id`);
+
+--
+-- Constraints for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD CONSTRAINT `wallet_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `wallet_requests`
+--
+ALTER TABLE `wallet_requests`
+  ADD CONSTRAINT `wallet_requests_ibfk_1` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`wallet_id`);
+
+--
+-- Constraints for table `wallet_transactions`
+--
+ALTER TABLE `wallet_transactions`
+  ADD CONSTRAINT `wallet_transactions_ibfk_1` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`wallet_id`);
