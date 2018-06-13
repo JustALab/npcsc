e30908f5b3e812503a1f30988d6dc62b7ce67d48
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-06-13 21:10:49 +0530

    train ticket card

--- pages/home.php
+++ pages/home.php
@@ -105,6 +105,22 @@
         </div>
       </div>
 
+      <div class="col-lg-3 col-6">
+        <!-- small card -->
+        <div class="small-box bg-secondary">
+          <div class="inner">
+            <h3>Train Ticket</h3>
+            <p>Book your air tickets along with web checkin</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-train"></i>
+          </div>
+          <a href="#" class="small-box-footer" onclick="comingsoon()">
+            Book now
+          <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
     </div>
   </section>
   <!-- /.content -->
