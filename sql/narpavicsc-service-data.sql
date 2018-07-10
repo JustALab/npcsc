d77a7809961fdd3b8625ccc594feb2c7674cf6f5
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-07-10 23:51:01 +0530

    sql commit

--- sql/narpavicsc-service-data.sql
+++ sql/narpavicsc-service-data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 08, 2018 at 08:38 AM
+-- Generation Time: Jul 10, 2018 at 08:11 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
@@ -14,13 +14,17 @@ SET time_zone = "+00:00";
 -- Database: `narpavicsc`
 --
 
+
 --
 -- Dumping data for table `services`
 --
 
 INSERT INTO `services` (`service_id`, `service_name`) VALUES
 (1, 'PAN Application'),
-(2, 'Passport Application');
+(2, 'Passport Application - Fresh/Reissue - 36 Pages'),
+(3, 'Passport Application - Fresh/Reissue - 60 Pages'),
+(4, 'Passport Application - Minors - Fresh/Reissue - 36 Pages'),
+(5, 'Passport Application - Tatkal');
 
 --
 -- Dumping data for table `price_config`
@@ -28,4 +32,7 @@ INSERT INTO `services` (`service_id`, `service_name`) VALUES
 
 INSERT INTO `price_config` (`price_config_id`, `service_id`, `amount`) VALUES
 (1, 1, '130'),
-(2, 2, '500');
+(2, 2, '1500'),
+(3, 3, '2000'),
+(4, 4, '1000'),
+(5, 5, '2000');
